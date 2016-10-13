ace9744361db63483ae3ced55d6a142d14efb779
王志成 <wangzhicheng@tongxinyiliao.com>
2016-10-13 14:41:36 +0800

    增加缓存

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict"
+let app = require('koa')()
+let path = require('path')
+let static_cache = require('koa-static-cache')
+let config = require('./config')
+
+// 默认路径
+app.use(function *(next){
+  if(this.path=="/")
+    this.path = config.index
+  yield next;
+});
+// 缓存
+app.use(static_cache(path.join(__dirname, config.root_dir), {
+  maxAge: config.max_storage_age
+}))
+// 启动
+app.listen(config.default_port)
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    index:  "/index.html",
+    default_port:  8080,
+    root_dir:  "public",
+    max_storage_age: 365 * 24 * 60 * 60
+};
