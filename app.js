4d0c399a1f7bd25883849ed2675c733a165fa3d0
王志成 <wangzhicheng@tongxinyiliao.com>
2016-10-14 17:14:13 +0800

    rm hook

--- app.js
+++ app.js
@@ -2,22 +2,12 @@
 let app = require('koa')()
 let path = require('path')
 let static_cache = require('koa-static-cache')
-let body = require('koa-body')
 let config = require('./config')
 
 // 处理post参数(this.request.body)
 app.use(body({formidable:{uploadDir: __dirname}}));
-// web-hooks
-app.use(function *(next){
-  if(this.request.method=="POST" && this.path=="/web_hooks"){
-    console.log(this.request.body);
-    this.body = "OK" 
-    return;
-  }
-});
 // 默认路径
 app.use(function *(next){
-  console.log(this.request.method=="POST");
   if(this.path=="/")
     this.path = config.index
   yield next;
